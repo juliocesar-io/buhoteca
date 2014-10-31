62369f807c43193ab0f27d592e21d3d4d85845b1
Julio César <julioc255io@gmail.com>
2014-10-30 23:12:08 -0500

    modificando formularios y validaciones

--- libros/forms.py
+++ libros/forms.py
@@ -2,9 +2,13 @@
 from django.forms import ModelForm
 from django import forms
 from .models import Libro
+from autores.models import Autor
 
 class LibroForm(forms.ModelForm):
-	titulo = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Javascipt: The Good Parts'}))
-	
+
+	#autores = list(Autor.objects.values('nombres'))
+
+	titulo = forms.CharField(widget=forms.TextInput(attrs={'class': 'error','placeholder': 'Javascipt: The Good Parts'}))
+	#autor = forms.ChoiceField(widget=forms.Select(attrs={'class': 'error'}), choices = autores)
     	class Meta:
         	model = Libro
