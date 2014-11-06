e31a385b78cca16c37f5462343b3a90c6b6443f6
Julio César <julioc255io@gmail.com>
2014-11-05 22:34:02 -0500

    Mirando modelos

--- libros/models.py
+++ libros/models.py
@@ -3,7 +3,7 @@ from autores.models import Autor
 
 class Libro(models.Model):
 	titulo = models.CharField(max_length=50, verbose_name='Titulo', unique=True, blank=False)
-	#area =  models.CharField(max_length=50, verbose_name='Area')
+	area =  models.CharField(max_length=50, verbose_name='Area', null=True, blank=True )
 	#editorial = models.CharField(max_length=50, verbose_name='Editorial')
 	#Relacion muchos a muchos el ORM de Django creara una tabla llamada "libro_autor" 
 	autor = models.ForeignKey(Autor)
