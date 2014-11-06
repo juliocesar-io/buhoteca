f2bdb6de9f54a39e42e151e9bbe84cdf7338f90e
Julio César <julioc255io@gmail.com>
2014-11-05 21:39:33 -0500

    Atributos modelo libro

--- libros/models.py
+++ libros/models.py
@@ -3,6 +3,9 @@ from autores.models import Autor
 
 class Libro(models.Model):
 	titulo = models.CharField(max_length=50, verbose_name='Titulo', unique=True, blank=False)
+	area =  models.CharField(max_length=50, verbose_name='Area', blank=False)
+	editorial = models.CharField(max_length=50, verbose_name='Editorial', blank=False)
+	#Relacion muchos a muchos
 	autor = models.ForeignKey(Autor)
 
 	def __unicode__(self):
