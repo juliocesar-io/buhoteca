5911249f9c729c5936f75b8b304cb6a4b4a9e19f
Julio César <julioc255io@gmail.com>
2014-11-11 22:41:58 -0500

    Documentando código

--- libros/models.py
+++ libros/models.py
@@ -4,22 +4,28 @@ from autores.models import Autor
 class Libro(models.Model):
 	titulo = models.CharField(max_length=50, verbose_name='Titulo', unique=True, blank=False)
 	area = models.CharField(max_length=50, verbose_name='Area', null=True)
-	editorial = models.CharField(max_length=50, verbose_name='Editorial', null=True)
-	cover_url = models.ImageField(upload_to='covers', verbose_name='Cover', blank=True, null=True)
-	digital_url = models.FileField(upload_to='digital', verbose_name='Archivo Digital', blank=True, null=True)
+	editorial = models.CharField(max_length=50, verbose_name='Editorial', null=True)		
 	disponible_fisico = models.BooleanField(verbose_name='Disponible fisico', default=False)
 	fecha_publicacion = models.DateField(verbose_name='Fecha de publicacion', null=True)
+	#Archivos asociados 
+	cover_url = models.ImageField(upload_to='covers', verbose_name='Cover', blank=True, null=True)
+	digital_url = models.FileField(upload_to='digital', verbose_name='Archivo Digital', blank=True, null=True)
+
+	#Restricciones de dominio, tradicionalmente se tenia que hacer otra tabla, con django podemos hacer un vector
+		#que contenga las elecciones.
 
 	FACULTAD_CHOICES = (
     	(u'1', u'Derecho'),
     	(u'2', u'Ingenieria De Sistemas'),
     	(u'3', u'Psicologia'),
+    	(u'4', u'Administracion de empresas'),
 	)
-
+	
 	facultad = models.CharField(max_length=1, choices=FACULTAD_CHOICES, null=True)
 
+	#Relacion muchos a muchos con "autores": el ORM de Django creara una tabla "libro_autor" que tiene como FK 
+		#las llaves primarias de libro y autor.
 
-	#Relacion muchos a muchos el ORM de Django creara una tabla llamada "libro_autor"
 	autor = models.ForeignKey(Autor)
 
 	def __unicode__(self):
