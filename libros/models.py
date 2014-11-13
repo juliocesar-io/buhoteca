380f49a61aa3cb4d298161bcc2cd38798426ebab
Julio César <julioc255io@gmail.com>
2014-11-12 22:19:08 -0500

    Clean/debuging Pycharm IDE

--- libros/models.py
+++ libros/models.py
@@ -1,30 +1,31 @@
 from django.db import models
 from autores.models import Autor
 
+
 class Libro(models.Model):
-	titulo = models.CharField(max_length=50, verbose_name='Titulo', unique=True, blank=False)
-	area = models.CharField(max_length=50, verbose_name='Area', null=True)
-	editorial = models.CharField(max_length=50, verbose_name='Editorial', null=True)		
-	disponible_fisico = models.BooleanField(verbose_name='Disponible fisico', default=False)
-	fecha_publicacion = models.DateField(verbose_name='Fecha de publicacion', null=True)
-	#Archivos asociados 
-	cover_url = models.ImageField(upload_to='covers', verbose_name='Cover', blank=True, null=True)
-	digital_url = models.FileField(upload_to='digital', verbose_name='Archivo Digital', blank=True, null=True)
+    titulo = models.CharField(max_length=50, verbose_name='Titulo', unique=True, blank=False)
+    area = models.CharField(max_length=50, verbose_name='Area', null=True)
+    editorial = models.CharField(max_length=50, verbose_name='Editorial', null=True)
+    disponible_fisico = models.BooleanField(verbose_name='Disponible fisico', default=False)
+    fecha_publicacion = models.DateField(verbose_name='Fecha de publicacion', null=True)
+    #Archivos asociados
+    cover_url = models.ImageField(upload_to='covers', verbose_name='Cover', blank=True, null=True)
+    digital_url = models.FileField(upload_to='digital', verbose_name='Archivo Digital', blank=True, null=True)
 
-	#Restricciones de dominio, tradicionalmente se tenia que hacer otra tabla, con django podemos hacer un vector
-		#que contenga las elecciones.
+    #Restricciones de dominio, tradicionalmente se tenia que hacer otra tabla, con django podemos hacer un vector
+    #que contenga las elecciones.
 
-	FACULTAD_CHOICES = (
-    	(u'1', u'Derecho'),
-    	(u'2', u'Ingenieria De Sistemas'),
-    	(u'3', u'Psicologia'),
-    	(u'4', u'Administracion de empresas'),
-	)
+    FACULTAD_CHOICES = (
+        (u'1', u'Derecho'),
+        (u'2', u'Ingenieria De Sistemas'),
+        (u'3', u'Psicologia'),
+        (u'4', u'Administracion de empresas'),
+    )
 
-	facultad = models.CharField(max_length=1, choices=FACULTAD_CHOICES, null=True)
+    facultad = models.CharField(max_length=1, choices=FACULTAD_CHOICES, null=True)
 
-	#Relacion muchos a muchos, para ver el codigo sql generado por este modelo ejecutar " $./manage.py sql libros"
-	autor = models.ForeignKey(Autor)
+    #Relacion muchos a muchos, para ver el codigo sql generado por este modelo ejecutar " $./manage.py sql libros"
+    autor = models.ForeignKey(Autor)
 
-	def __unicode__(self):
- 		return self.titulo
+    def __unicode__(self):
+        return self.titulo
--- libros/views.py
+++ libros/views.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
+
 from django.template import RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
 from django.shortcuts import render
@@ -8,32 +8,34 @@ from .models import Libro
 from autores.models import Autor
 from .forms import LibroForm
 
+
 def inicio(request):
-	autores = Autor.objects.all()
-	libros = Libro.objects.all()
-        return render(request,'inicio.html', {'autores':autores,'libros':libros})
+    autores = Autor.objects.all()
+    libros = Libro.objects.all()
+    return render(request, 'inicio.html', {'autores': autores, 'libros': libros})
 
 
 def nuevo_libro(request, template='libroForm.html'):
     form = LibroForm()
     if request.method == "POST":
-    	form = LibroForm(request.POST, request.FILES)
+        form = LibroForm(request.POST, request.FILES)
         if form.is_valid():
-        	form.save()
-		return render(request,'libroNuevo.html')
+            form.save()
+        return render(request, 'libroNuevo.html')
 
     kwvars = {
         "form": form,
     }
     return render_to_response(template, kwvars, context_instance=RequestContext(request))
 
+
 def listado_libros(request):
     autores = Autor.objects.all()
     libros = Libro.objects.all()
-    return render(request,'librosListado.html', {'autores':autores,'libros':libros})
+    return render(request, 'librosListado.html', {'autores': autores, 'libros': libros})
 
 
 def detalle_libro(request, id_libro, template='libroDetalle.html'):
-    libro = get_object_or_404(Libro, pk = id_libro)
+    libro = get_object_or_404(Libro, pk=id_libro)
     return render_to_response(template, {'libro': libro}, context_instance=RequestContext(request))
 
